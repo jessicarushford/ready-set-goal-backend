430f2d18504131f89eaa9e7adb880c958b84f1af
Isaiah <isaiah.sweezie@gmail.com>
2022-04-08 17:01:51 -0400

    backend changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,10 +3,12 @@ import express from "express";
 import cors from "cors";
 import goalRouter from "./routes/goalRouter";
 import userRouter from "./routes/userRouter";
+import zenRouter from "./routes/zenRouter";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/goals", goalRouter);
 app.use("/users", userRouter);
+app.use("/", zenRouter);
 export const api = functions.https.onRequest(app);
